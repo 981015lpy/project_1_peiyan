8e958f00c4068cfec8326be0605a661c1272ffef
pl1705@nyu.edu <pl1705@nyu.edu>
2020-10-24 02:12:24 +0800

    add person walking animation

--- src/assignment.js
+++ src/assignment.js
@@ -28,29 +28,30 @@ export default class Assignment extends React.Component {
       p5.resizeCanvas(canvasWidth, canvasHeight)
     }
 
-    let mountainMoveSpeed = 1
-    let mountainPoint = Math.random()
+    let count = 1
 
     let personPosition = { x: canvasWidth / 2, y: canvasHeight - 100}
     
     const draw = (p5) => {
       p5.background(210)
       
-      
+      let second = parseInt(p5.millis() / 1000)
+
       // create the sun
       sun(p5, 400, 250, 150)
 
       // create the mountain group
-      setTimeout(() => {
-        mountainPoint -= mountainMoveSpeed
-      }, 10)
-      mountain(p5, canvasWidth, canvasHeight, 600, 5, mountainPoint)
+      mountain(p5, canvasWidth, canvasHeight, 600, 5, 20)
 
       // create the black ground
       ground(p5, { x: 0, y: canvasHeight - 50 }, canvasWidth, 50)
 
-      // create person
+      p5.push()
       person(p5, personPosition)
+      p5.pop()
+
+      // create person
+      // if (second > 0) person(p5, personPosition)
     }
 
     return (
--- src/components/person.js
+++ src/components/person.js
@@ -1,35 +1,50 @@
 export default function person(p5, position) {
   p5.push()
+
+  let t = p5.millis() / 1000
+  // let t = 1
+  let speed = 2
   
   let headPosition = { x: position.x, y: position.y - 20 }
+
   let body = {
     startPoint: { x: position.x, y: position.y - 20 },
     endPoint: { x: headPosition.x, y: headPosition.y + 50}
   }
   let arm_front = {
     startPoint: { x: position.x, y: position.y + 10 },
-    endPoint: { x: position.x + 35, y: position.y + 25 }
+    endPoint: { x: position.x  + 30 * Math.sin( -speed * t), y: position.y + 25 }
   }
   let arm_back = {
     startPoint: { x: position.x, y: position.y + 10 },
-    endPoint: { x: position.x - 25, y: position.y + 35 }
+    endPoint: { x: position.x - 30 * Math.sin( -speed * t), y: position.y + 25 }
   }
 
-  let leg_front = {
+  let leg_front_1 = {
     startPoint: { x: position.x, y: position.y + 20 + 10 },
-    endPoint: { x: position.x + 25, y: position.y + 20 + 45 }
+    endPoint: { x: position.x + 5 * Math.sin( -speed * t), y: position.y + 20 + 25 }
+  }
+  let leg_front_2 = {
+    startPoint: { x: leg_front_1.endPoint.x, y: leg_front_1.endPoint.y },
+    endPoint: { x: leg_front_1.endPoint.x - 10, y: leg_front_1.endPoint.y + 20 }
   }
 
-  let leg_back = {
+  let leg_back_1 = {
     startPoint: { x: position.x, y: position.y + 20 + 10 },
-    endPoint: { x: position.x - 25, y: position.y + 20 + 45 }
+    endPoint: { x: position.x + 15, y: position.y + 40 - 5 * Math.sin( speed * t) }
+  }
+
+  let leg_back_2 = {
+    startPoint: { x: leg_back_1.endPoint.x, y: leg_back_1.endPoint.y },
+    endPoint: { x: leg_back_1.endPoint.x - 10 , y: leg_front_1.endPoint.y + 20 + Math.sin( 2 * speed * t) }
   }
 
   // create front arm
   createLine(arm_front, 140, 5)
 
   // create front leg
-  createLine(leg_front, 140, 5)
+  createLine(leg_front_1, 140, 5)
+  createLine(leg_front_2, 140, 5)
 
   // create body
   createLine(body, 140, 10)
@@ -43,7 +58,8 @@ export default function person(p5, position) {
   createLine(arm_back, 140, 5)
 
   // create back leg
-  createLine(leg_back, 140, 5)
+  createLine(leg_back_1, 140, 5)
+  createLine(leg_back_2, 140, 5)
 
 
   p5.pop()
